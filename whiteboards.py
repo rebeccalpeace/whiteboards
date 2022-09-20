d0203aea258098791179cf70dd154d2a32de09c2
rebeccalpeace <rebeccalpeace@gmail.com>
2022-09-20 12:58:35 -0400

    isAnagram leetcode

--- whiteboards.py
+++ whiteboards.py
@@ -2,16 +2,22 @@
 
 # Given an array of integers (nums), which is sorted in ascending order, and a target integer, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
-nums1 = [-1, 0, 3, 5, 9, 12]
+# nums1 = [-1, 0, 3, 5, 9, 12]
 
-target = 9
-target2 = 2
+# target = 9
+# target2 = 2
 
-def find_index(nums, target):
-    for i in nums:
-        if i == target:
-            return nums.index(i)
-        elif i > target:
-            return -1
+# def find_index(nums, target):
+#     for i in nums:
+#         if i == target:
+#             return nums.index(i)
+#         elif i > target:
+#             return -1
     
-print(find_index(nums1, 2))
+# print(find_index(nums1, 2))
+
+
+def isAnagram(s, t):
+    return sorted(s) == sorted(t)
+
+print(isAnagram('rat', 'car'))
