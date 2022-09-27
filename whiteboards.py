c5af4c79a7383d31d99eeda8f335cac63c39d94e
rebeccalpeace <rebeccalpeace@gmail.com>
2022-09-27 15:04:29 -0400

    daily challenge 9/27/22

--- whiteboards.py
+++ whiteboards.py
@@ -152,7 +152,29 @@
 # print(valid_parentheses("()test()"))
 
 
+# You are given a string s representing an attendance record for a student where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
+
+# 'A': Absent.
+# 'L': Late.
+# 'P': Present.
+
+# The student is eligible for an attendance award if they meet both of the following criteria:
+# The student was absent ('A') for strictly fewer than 2 days total.
+# The student was never late ('L') for 3 or more consecutivedays.
+# Return true if the student is eligible for an attendance award, or false otherwise.
+
+# s = "PPALLP"
+s = "PPALLL"
+
+def award(str1):
+    for i in range(len(str1)):
+        if str1[i] == 'L':
+            if str1[i+1] == 'L' and str1[i+2] == 'L':
+                return False
+    if str1.count('A') < 2:
+        return True
+    return False
 
 
-
+print(award(s))
 
